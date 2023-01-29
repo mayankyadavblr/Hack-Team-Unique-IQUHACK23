a3bbf6f903127f3b0cc80a2c16e459d8da94affa
mayankyadavblr <prehistoric0001@gmail.com>
2023-01-29 03:27:49 -0500

    making plot to show on website

--- display_network.py
+++ display_network.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import pandas as pd
 from radius_of_infection import *
+import matplotlib.pyplot as plt
 
 data=prepare_dataframe('Book1.xlsx')
 final_dataframe=infection_circle(data,2)
@@ -17,3 +18,5 @@ print(edges_lst)
 network=nx.Graph()
 network.add_edges_from(edges_lst)
 nx.draw(network, with_labels = True, node_color='#7ab8f5', width=2, font_size=16, node_size=700, pos=nx.planar_layout(network))
+plt.show()
+plt.savefig('plotgraph.png', dpi=300, bbox_inches='tight')
